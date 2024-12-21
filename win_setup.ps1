e9b347f0571d7b8b82c07c6aca938f8b21278d3e
Joshua <jperrett256@gmail.com>
2024-12-21 16:59:25 +0000

    win32 setup script support user paths, added msvc related submodules

--- win_setup.ps1
+++ win_setup.ps1
@@ -2,19 +2,33 @@ function AddToPath
 {
     param
     (
-        [string]$Dir
+        [Parameter(Mandatory)]
+        [string]$DirPath,
+        [bool]$SystemWide = $false
     )
-    if (!(Test-Path $Dir) )
+    $env_var_target = If ($SystemWide) {"Machine"} Else {"User"}
+
+    Write-Host "Adding ""$DirPath"" to $env_var_target PATH"
+
+    if (!(Test-Path $DirPath) )
     {
-        Write-warning "Supplied directory was not found!"
+        Write-Warning "Supplied directory was not found!"
         return
     }
-    $PATH = [Environment]::GetEnvironmentVariable("PATH", "Machine")
-    if ($PATH -notlike "*"+$Dir+"*")
+
+    $path = [Environment]::GetEnvironmentVariable("PATH", $env_var_target)
+    if ($path -notlike "*"+$DirPath+"*")
     {
-        [Environment]::SetEnvironmentVariable("PATH", "$PATH;$Dir", "Machine")
+        [Environment]::SetEnvironmentVariable("PATH", "$path;$DirPath", $env_var_target)
+        # TODO return true if just added to path
     }
 }
 
-AddToPath("C:\Program Files\Sublime Text")
-AddToPath("C:\Program Files\Sublime Merge")
+AddToPath -Dir "C:\Program Files\Sublime Text" -SystemWide $true
+AddToPath -Dir "C:\Program Files\Sublime Merge" -SystemWide $true
+
+$user_profile = [Environment]::GetEnvironmentVariable("USERPROFILE")
+
+AddToPath -Dir $user_profile"\miniforge3\condabin"
+# TODO if added to path, refresh process PATH and run:
+# cmd.exe /c conda create -n default python numpy pandas scipy # PyPDF2 python-pptx
